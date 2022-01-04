4c259ef17fbb28e5d95733e487b706c982191532
Max Mezalon <jmezalon@gmail.com>
2022-01-04 10:57:00 -0500

    commit tree 6937db111bf0a2391254c7a0555c3d97b3f290d5

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,29 @@
 require_relative './stack'
 
 # your code here
+class MyQueue
+
+    def initialize
+        @s1 = Stack.new 
+        @s2 = Stack.new
+    end
+
+    def add(value)
+        @s2 = @s1.push(value)    
+    end
+
+    def peek 
+        @s2.first
+    end
+
+    def remove  
+        @s2.shift
+    end
+end
+
+
+mq = MyQueue.new
+
+mq.add(1)
+
+puts mq.peek
